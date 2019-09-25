2a7b75c7751bff779613c871c386cca3d7573630
长风 <across_horizon@qq.com>
2019-09-25 15:45:00 +0800

    Update IdGenerator.kt

--- UdidServer/src/db/IdGenerator.kt
+++ UdidServer/src/db/IdGenerator.kt
@@ -6,7 +6,8 @@ import config.ConfigManager
  * ID的构造 服务ID+自增序列
  * 最高位(符号位）保留
  * 服务ID有15bit， 取值范围：[0,32767)
- * 序列有40bit(取值范围约一万亿）
+ * 序列有48bit
+ * 可以根据情况具体分配（一共64bit）
  */
 object IdGenerator {
     private const val INIT_ID = 1L shl 20
@@ -20,11 +21,11 @@ object IdGenerator {
             if(serverId >= 32767){
                 throw IllegalStateException("server id must less then 32767")
             }
-            IdSequenceKv.deviceIdSeq =(serverId shl 40) + INIT_ID
+            IdSequenceKv.deviceIdSeq =(serverId shl 48) + INIT_ID
         }else{
             // 因为 IdSequenceKv 非阻塞式提交，所以假如宕机的话有可能会丢失更新
             // 所以可以在初始化的时候丢弃 FLUSH_GAP 个ID，以避免ID重复
-            // 假设平均每天服务重启5次，则每天会丢弃1000个ID，一年30W+个，相对万亿级别的ID范围，可以忽略不计
+            // 假设平均每天服务重启5次，则平均每天会丢弃1000个ID，一年30W+个，相对百万亿级别的ID范围，可以忽略不计
             IdSequenceKv.deviceIdSeq += FLUSH_GAP
         }
     }
